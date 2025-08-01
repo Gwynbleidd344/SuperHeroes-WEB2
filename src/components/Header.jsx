694444f62eba4d64c123e0a7300e899f57af0304
Vaniaina <hei.vaniaina@gmail.com>
2025-08-01 13:09:39 +0300

    ADD: navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/image/heroes-logo.png'
+export default function Header() {
+    return (
+        <header className='flex row justify-between bg-indigo-900 p-4'>
+            <div className='flex row'>
+                <img src={logo} alt="logo" className='w-12'/>
+                <h1 className='hidden sm:inline'>SuperHero Catalog</h1>
+            </div>
+            <input type="text" placeholder="Search ..." className='w-7/15 bg-white rounded-xl p-2.5'/>
+        </header>
+    )
+}
